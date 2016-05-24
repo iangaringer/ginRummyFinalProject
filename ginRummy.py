9082a025d4f4b0b8d96b1547db382c5a581a8706
GouwarPower <jgouwar@gmail.com>
2016-05-24 13:07:46 -0400

    Update ginRummy.py

--- ginRummy.py
+++ ginRummy.py
@@ -2,5 +2,14 @@
 #game.py
 #May 24 2016
 #This is the main game of gin rummy 
+from Deck import *
+from Player import *
+from 
 def main():
+	print("Welcome to the Gin Rummy Extravaganza! Press Enter to continue")
+	input()
+	player1Name = input("What is player 1's name?: ")
+	player2Name = input("What is player 2's name?: ")
+	Player1 = Player(player1Name); Player2 = Player(player2Name)
+	
   
